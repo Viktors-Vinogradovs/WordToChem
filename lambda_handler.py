41ea2f9f98b4cc2fef121be8830356c206416dda
Viktors Vinogradovs <165190028+Viktors-Vinogradovs@users.noreply.github.com>
2025-12-13 12:44:00 +0200

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,10 +2,3 @@ from mangum import Mangum
 from app import app
 
 handler = Mangum(app, lifespan="off")
-```
-
-### File 2: `requirements.txt`
-Create or update this file with:
-```
-Flask==3.0.0
-mangum==0.17.0
